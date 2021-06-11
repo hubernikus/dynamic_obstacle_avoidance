fix weighting function for multiple boundaries

src/dynamic_obstacle_avoidance/obstacles/ellipse.py
@@ -257,8 +257,21 @@ class Ellipse(Obstacle):
         
         return abs(angle_tang-(angle_tang1_pos+angle_pos_tang0)) < margin_subtraction
 
-    def get_gamma(self, position, in_global_frame=False, gamma_type=None, gamma_distance=None):
-        """ Gamma of ellipse 3d"""
+    def get_gamma(self, position, in_global_frame=False, gamma_type=None, gamma_distance=None,
+                  inverted=None):
+        """ Returns gamma value of an ellipse shaped obstacle at position
+
+        Property
+        -------
+        position: array like position of size (dimension,)
+        in_global_frame: If position input is in global frame, transform to local frame
+        gamma_type: Different types of the distance measure-evaluation
+        inverted: Enforce normal / inverted evaluation (if None use the object / boundary default)
+
+        Return
+        ------
+        Gamma: distance value gamma of float
+        """
 
         # WHY WAS THIS ACTIVE?!?!
         # if self.dim==2:
@@ -280,6 +293,9 @@ class Ellipse(Obstacle):
             
         Gamma = np.sum((np.abs(position)/self.axes_with_margin)**(2*self.curvature))
 
+        if self.is_boundary:
+            Gamma = 1./Gamma
+            
         return Gamma
 
     def get_normal_ellipse(self, position):

src/dynamic_obstacle_avoidance/obstacles/multiboundary_container.py
@@ -11,6 +11,8 @@ import numpy as np
 
 from dynamic_obstacle_avoidance.obstacles import BaseContainer
 
+import matplotlib.pyplot as plt     # Debuggin only
+
 
 class MultiBoundaryContainer(BaseContainer):
     """ Container to treat multiple boundaries / walls. """
@@ -24,38 +26,39 @@ class MultiBoundaryContainer(BaseContainer):
 
         ind_inside = gamma_list > 1
         ind_close = gamma_list > gamma_margin_close_wall
+        num_close = np.sum(ind_close)
             
-        num_close_obs = np.sum(ind_inside)
-        
-        for ii, ii_self in zip(range(num_close_obs), np.arange(self.n_obstacles)[ind_close]):
+        for ii, ii_self in zip(range(np.sum(ind_inside)), np.arange(self.n_obstacles)[ind_inside]):
             # Displacement_weight for each obstacle
             # TODO: make sure following function is for obstacles other than ellipses (!)
             boundary_point = self[ii_self].get_intersection_with_surface(
                 direction=(position - self[ii_self].center_position), in_global_frame=True)
 
-            weights = np.zeros(num_close_obs)
-            
-            for jj, jj_self in zip(range(num_close_obs), np.arange(self.n_obstacles)[ind_close]):
+            weights = np.zeros(num_close)
+            for jj, jj_self in zip(range(num_close), np.arange(self.n_obstacles)[ind_close]):
                 if ii_self == jj_self:
                     continue
-
-                gamma_boundary_point = self[jj_self].get_gamma(
-                    boundary_point, in_global_frame=True)
-
+                gamma_boundary_point = self[jj_self].get_gamma(boundary_point, in_global_frame=True)
                 if gamma_boundary_point < 1:
                     # Only obstacles are considered which intersect at the (projected) boundar point
                     continue
 
-                gamma_center_point = self[jj_self].get_gamma(
-                    self[ii_self].center_point, in_global_frame=True)
+                vec_boundary = boundary_point - self[ii_self].center_position
+                dist_boundary_point = np.linalg.norm(vec_boundary)
+                vec_boundary = vec_boundary / dist_boundary_point
+                
+                dist_point = np.dot(position-self[ii_self].center_position, vec_boundary)
 
-                weight_1 = ((gamma_reference_point - gamma_list[ii_self])
-                            / (gamma_reference_point - gamma_boundary_point))
+                gamma_center_point = self[jj_self].get_gamma(
+                    self[ii_self].center_position, in_global_frame=True)
+
+                weight_1 = (dist_point)/(dist_boundary_point-dist_point)
                 weight_2 = gamma_boundary_point - 1
 
-                weights[jj] = 1 / (1 + weight_1*weight_2)
+                weights[jj] = 1-1 / (1 + weight_1*weight_2)
 
             rel_reference_weight = np.max(weights)
+            # breakpoint()
 
             if rel_reference_weight > 1:
                 # TODO: remove aftr debugging..
@@ -63,9 +66,11 @@ class MultiBoundaryContainer(BaseContainer):
                 raise ValueError("Weight greater than 1...")
 
             self[ii_self].global_relative_reference_point = (
-                rel_reference_weight*position + (1 - rel_reference_weight)*self[ii_self].global_reference_point)
+                rel_reference_weight*position +
+                (1 - rel_reference_weight)*self[ii_self].global_reference_point)
             
-        for ii_self in np.arange(self.n_obstacles)[~ind_close]:
+
+        for ii_self in np.arange(self.n_obstacles)[~ind_inside]:
             self[ii_self].reset_relative_reference()
 
     def test(self):

tests/test_multiwall_reference_point.py
@@ -43,21 +43,14 @@ class TestMultiBoundary(unittest.TestCase):
         self.assertTrue(True)
 
     @classmethod
-    def plottest_list(cls):
+    def plottest_list_simple(cls):
         """ Additional test for visualization. """
         
         import matplotlib.pyplot as plt
         from dynamic_obstacle_avoidance.visualization import plot_obstacles
+        plt.close('all')
 
         obs_list = MultiBoundaryContainer()
-        obs_list.append(
-            Ellipse(
-            center_position=np.array([-6, 0]), 
-            axes_length=np.array([5, 2]),
-            orientation=50./180*pi,
-            is_boundary=True,
-            )
-        )
         obs_list.append(
             Ellipse(
             center_position=np.array([0, 0]), 
@@ -66,18 +59,155 @@ class TestMultiBoundary(unittest.TestCase):
             is_boundary=True,
             )
         )
+        obs_list.append(
+            Ellipse(
+            center_position=np.array([6, 0]), 
+            axes_length=np.array([5, 2]),
+            orientation=50./180*pi,
+            is_boundary=True,
+            )
+        )
 
-        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
-        plot_obstacles(ax=ax, obs=obs_list, x_range=[-10, 5], y_range=[-5, 5])
+        position_list = [
+            np.array([2, -2]),
+            np.array([0, -2]),
+            np.array([3, -2]),
+            np.array([-2, 2]),
+            ]
+        n_tests = len(position_list)
+        
+        fig, axs = plt.subplots(1, n_tests, figsize=(14, 5))
+        
+        for ii in range(n_tests):
+            ax = axs[ii]
+            plot_obstacles(ax=ax, obs=obs_list, x_range=[-6, 10], y_range=[-6, 6], showLabel=False)
+            position = position_list[ii]
+            obs_list.update_relative_reference_point(position)
+            
+            ax.plot(position[0], position[1], 'ko')
 
-        position = np.array([-8, -2])
+            for oo in range(len(obs_list)):
+                abs_ref_point = obs_list[oo].global_reference_point
+                ax.plot(abs_ref_point[0], abs_ref_point[1], 'k+')
 
-        obs_list.update_relative_reference_point(position)
+                ref_point = obs_list[oo].global_relative_reference_point
 
-        plt.plot(position[0], position[1], 'ko')
-        for oo in range(len(obs_list)):
-            ref_point = obs_list[oo].global_relative_reference_point
-            plt.plot(ref_point[0], ref_point[1], 'k*')
+                ax.plot(ref_point[0], ref_point[1], 'k*')
+                ax.plot([abs_ref_point[0], ref_point[0]],
+                         [abs_ref_point[1], ref_point[1]], 'k--')
+
+    @classmethod
+    def plottest_list_advanced(cls):
+        """ Additional test for visualization. """
+        
+        import matplotlib.pyplot as plt
+        from dynamic_obstacle_avoidance.visualization import plot_obstacles
+
+        obs_list = MultiBoundaryContainer()
+        obs_list.append(
+            Ellipse(
+            center_position=np.array([0, 0]), 
+            axes_length=np.array([6, 2]),
+            orientation=-40./180*pi,
+            is_boundary=True,
+            )
+        )
+        obs_list.append(
+            Ellipse(
+            center_position=np.array([5, 0]), 
+            axes_length=np.array([6, 2]),
+            orientation=40./180*pi,
+            is_boundary=True,
+            )
+        )
+
+        position_list = [
+            np.array([4, -2]),
+            np.array([4, -4]),
+            np.array([3.458, -3.1948]),
+            ]
+        n_tests = len(position_list)
+        
+        fig, axs = plt.subplots(1, n_tests, figsize=(14, 5))
+        
+        for ii in range(n_tests):
+            ax = axs[ii]
+            plot_obstacles(ax=ax, obs=obs_list, x_range=[-6, 10], y_range=[-6, 6], showLabel=False)
+            position = position_list[ii]
+            obs_list.update_relative_reference_point(position)
+            
+            ax.plot(position[0], position[1], 'ko')
+
+            for oo in range(len(obs_list)):
+                abs_ref_point = obs_list[oo].global_reference_point
+                ax.plot(abs_ref_point[0], abs_ref_point[1], 'k+')
+
+                ref_point = obs_list[oo].global_relative_reference_point
+
+                ax.plot(ref_point[0], ref_point[1], 'k*')
+                ax.plot([abs_ref_point[0], ref_point[0]],
+                         [abs_ref_point[1], ref_point[1]], 'k--')
+
+
+    @classmethod
+    def plottest_list_intersect(cls):
+        """ Additional test for visualization. """
+        
+        import matplotlib.pyplot as plt
+        from dynamic_obstacle_avoidance.visualization import plot_obstacles
+
+        obs_list = MultiBoundaryContainer()
+        obs_list.append(
+            Ellipse(
+            center_position=np.array([0, 0]), 
+            axes_length=np.array([6, 3]),
+            orientation=-45./180*pi,
+            is_boundary=True,
+            )
+        )
+        obs_list.append(
+            Ellipse(
+            center_position=np.array([0, 0]), 
+            axes_length=np.array([6, 3]),
+            orientation=45./180*pi,
+            is_boundary=True,
+            )
+        )
+
+        position_list = [
+            # np.array([2.57, -1.20]),
+            np.array([-2.57, -1.20]),
+            # np.array([-1.20, 2.57]),
+            # np.array([4, -2]),
+            np.array([0.0245, -3.31]),
+            ]
+        n_tests = len(position_list)
+        
+        fig, axs = plt.subplots(1, n_tests, figsize=(14, 5))
+        
+        for ii in range(n_tests):
+            try:
+                ax = axs[ii]
+            except TypeError:
+                # If it's a single element; not a list.
+                ax = axs 
+                
+            plot_obstacles(ax=ax, obs=obs_list, x_range=[-7, 7], y_range=[-6, 6], showLabel=False)
+            position = position_list[ii]
+            obs_list.update_relative_reference_point(position)
+            
+            ax.plot(position[0], position[1], 'ko')
+
+            for oo in range(len(obs_list)):
+                abs_ref_point = obs_list[oo].global_reference_point
+                ax.plot(abs_ref_point[0], abs_ref_point[1], 'k+')
+
+                ref_point = obs_list[oo].global_relative_reference_point
+
+                ax.plot(ref_point[0], ref_point[1], 'k*')
+                ax.plot([abs_ref_point[0], ref_point[0]],
+                         [abs_ref_point[1], ref_point[1]], 'k--')
+
 
 if __name__ == '__main__':
     # Allow running in ipython (!)
@@ -86,5 +216,6 @@ if __name__ == '__main__':
 
     visualize = True
     if visualize:
-        TestMultiBoundary.plottest_list()
-        
+        TestMultiBoundary.plottest_list_simple()
+        TestMultiBoundary.plottest_list_advanced()
+        TestMultiBoundary.plottest_list_intersect()