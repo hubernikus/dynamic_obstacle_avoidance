ac5dbd157d6024bd474aed87ad2e6037edded5a1
Lukas <huberniku@gmail.com>
2021-06-29 23:27:35 +0200

    new visualization & testing scripts

--- scripts/example_3d_rotation.py
+++ scripts/example_3d_rotation.py
@@ -9,27 +9,36 @@ import copy
 import numpy as np
 from numpy import pi
 
-from dynamic_obstacle_avoidance.containers import MultiBoundaryContainer
 from dynamic_obstacle_avoidance.obstacles import Ellipse
+from dynamic_obstacle_avoidance.containers import MultiBoundaryContainer, RotationContainer
 from dynamic_obstacle_avoidance.visualization.plot_3d_trajectory import plot_obstacles_and_trajectory_3d
 
-def test_single_ellipse_3d(self):
-    obs_list = BaseContainer()
-    
-    obs_list.append(
+def test_single_ellipse_3d():
+    ObstacleEnvironment = RotationContainer()
+    ObstacleEnvironment.append(
         Ellipse(
         center_position=np.array([0, 1, 0]), 
-        axes_length=np.array([1, 1, 1]),
+        axes_length=np.array([0.1, 0.3, 0.2]),
         orientation=[0, 0, 0], # Set orientation as euler
         tail_effect=False,
         )
     )
-    plot_obstacles_and_trajectory_3d()
-    pass
+    ObstacleEnvironment.append(
+        Ellipse(
+        center_position=np.array([-0.5, 1, 1]), 
+        axes_length=np.array([0.2, 0.2, 0.1]),
+        orientation=[0, 0, 0], # Set orientation as euler
+        tail_effect=False,
+        )
+    )
+    xyz_lim=[-1.5, 1.5]
+    # plot_obstacles_and_trajectory_3d(ObstacleEnvironment)
+    plot_obstacles_and_trajectory_3d(
+        ObstacleEnvironment,
+        x_lim=xyz_lim, y_lim=xyz_lim, z_lim=xyz_lim)
+                          # x_lim=[-2, 2], y_lim=[-2, 2], z_lim=[-2, 2])
     
-    
-
 if (__name__)=="__main__":
-    test_ellipse_3d()
+    test_single_ellipse_3d()
     
 
--- scripts/example_rotation_modulation.py
+++ scripts/example_rotation_modulation.py
@@ -2,7 +2,6 @@
 """ Script to show lab environment on computer """
 # Author: Lukas Huber
 # License: BSD (c) 2021
-
 import warnings
 import copy
 
@@ -10,9 +9,9 @@ import numpy as np
 from numpy import pi
 import matplotlib.pyplot as plt
 
-# from vartools.dynamicalsys.closedform import ds_quadratic_axis_convergence
-# from vartools.dynamicalsys.closedform import evaluate_linear_dynamical_system
 from vartools.dynamical_systems import LinearSystem, QuadraticAxisConvergence
+from vartools.dynamical_systems import BifurcationSpiral
+from vartools.dynamical_systems import plot_dynamical_system_streamplot
 
 from dynamic_obstacle_avoidance.containers import BaseContainer, MultiBoundaryContainer, RotationContainer
 from dynamic_obstacle_avoidance.obstacles import Ellipse, StarshapedFlower
@@ -26,7 +25,6 @@ plt.ion()
 
 def single_ellipse(rot_degree=0.):
     obs_list = RotationContainer()
-    
     obs_list.append(
         Ellipse(
         center_position=np.array([0, 0]), 
@@ -232,14 +230,13 @@ def single_ellipse_spiral_triple_plot(save_figure=False, n_resolution=40):
     x_lim = [-10, 10]
     y_lim = [-10, 10]
     
-    InitialSystem = LinearSystem(attractor_position=np.array([0, -4]),
+    InitialSystem = LinearSystem(attractor_position=np.array([0, -5]),
                                  A_matrix=np.array([[-1.0, -3.0],
-                                                    [3.0,-1.0]])
-                                 )
+                                                    [3.0,-1.0]]))
 
-    # fig, axs = plt.subplots(1, 3, figsize=(15, 6))
-    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-    axs = [None, None, ax]
+    fig, axs = plt.subplots(1, 3, figsize=(15, 6))
+    # fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    # axs = [None, None, ax]
 
     obstacle_list = single_ellipse(rot_degree=30)
     obstacle_list.set_convergence_directions(InitialSystem)
@@ -259,8 +256,8 @@ def single_ellipse_spiral_triple_plot(save_figure=False, n_resolution=40):
         # show_streamplot=False,       
         )
 
-    if True:
-        return
+    # if True:
+        # return
 
     obstacle_list = single_ellipse(rot_degree=30)
     Simulation_vectorFields(
@@ -294,7 +291,198 @@ def single_ellipse_spiral_triple_plot(save_figure=False, n_resolution=40):
         # show_streamplot=False,       
         )
 
+    if save_figure:
+        figure_name = "spiral_single_ellipse"
+        plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
+
+
+def single_ellipse_spiral_analysis(save_figure=False, n_resolution=40):
+    x_lim = [-10, 10]
+    y_lim = [-10, 10]
     
+    InitialSystem = LinearSystem(attractor_position=np.array([0, -5]),
+                                 A_matrix=np.array([[-1.0, -3.0],
+                                                    [3.0,-1.0]]))
+
+    fig, axs = plt.subplots(1, 3, figsize=(15, 6))
+    # fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    # axs = [None, None, ax]
+
+    obstacle_list = single_ellipse(rot_degree=30)
+    obstacle_list.set_convergence_directions(InitialSystem)
+
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[2]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    plot_dynamical_system_streamplot(DynamicalSystem=obstacle_list._ConvergenceDynamics[0],
+                                     x_lim=x_lim, y_lim=y_lim, axes_equal=True,
+                                     fig_ax_handle=(fig, axs[1]), n_resolution=n_resolution)
+    plot_obstacles(ax=axs[1], obs=obstacle_list, x_range=x_lim, y_range=y_lim, noTicks=True, showLabel=False,
+                   alpha_obstacle=0.4)
+
+    obstacle_list = []
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[0]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    if save_figure:
+        figure_name = "spiral_single_ellipse_analysis"
+        plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
+
+
+def single_ellipse_multiattractor_triple_plot(save_figure=False, n_resolution=40):
+    x_lim = [-10, 10]
+    y_lim = [-6, 14]
+
+    InitialSystem = BifurcationSpiral(maximum_velocity=1)
+
+    obstacle_list = RotationContainer()
+    obstacle_list.append(
+        Ellipse(
+        center_position=np.array([1, 6]), 
+        axes_length=np.array([2, 5]),
+        orientation=80./180.*pi))
+    obstacle_list.set_convergence_directions(InitialSystem)
+
+    fig, axs = plt.subplots(1, 3, figsize=(15, 6))
+    # fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    # axs = [None, None, ax]
+    
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[2]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    # if True:
+        # return
+
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        # obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[1]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    obstacle_list = []
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[0]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    if save_figure:
+        figure_name = "multiattractor_single_ellipse"
+        plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
+
+
+def single_ellipse_multiattractor_analysis(save_figure=False, n_resolution=40):
+    x_lim = [-10, 10]
+    y_lim = [-10, 10]
+
+    fig, axs = plt.subplots(1, 3, figsize=(15, 6))
+    # fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    # axs = [None, None, ax]
+    InitialSystem = BifurcationSpiral(maximum_velocity=1)
+
+    obstacle_list = RotationContainer()
+    obstacle_list.append(
+        Ellipse(
+        center_position=np.array([1, 6]), 
+        axes_length=np.array([2, 5]),
+        orientation=80./180.*pi))
+    obstacle_list.set_convergence_directions(InitialSystem)
+
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[2]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    plot_dynamical_system_streamplot(DynamicalSystem=obstacle_list._ConvergenceDynamics[0],
+                                     x_lim=x_lim, y_lim=y_lim, axes_equal=True,
+                                     fig_ax_handle=(fig, axs[1]), n_resolution=n_resolution)
+    plot_obstacles(ax=axs[1], obs=obstacle_list, x_range=x_lim, y_range=y_lim, noTicks=True, showLabel=False,
+                   alpha_obstacle=0.4)
+
+    obstacle_list = []
+    Simulation_vectorFields(
+        x_lim, y_lim, n_resolution, obstacle_list,
+        saveFigure=False, 
+        noTicks=True, showLabel=False,
+        draw_vectorField=True,
+        dynamical_system=InitialSystem.evaluate,
+        obs_avoidance_func=obstacle_avoidance_rotational,
+        automatic_reference_point=False,
+        pos_attractor=InitialSystem.attractor_position,
+        fig_and_ax_handle=(fig, axs[0]),
+        # Quiver or Streamplot
+        show_streamplot=True,
+        # show_streamplot=False,       
+        )
+
+    if save_figure:
+        figure_name = "spiral_multiattractor_analysis"
+        plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
+
 
 def single_ellipse_hull_linear_triple_plot(save_figure=False, n_resolution=40):
     """ Moving inside an 'ellipse hull with linear dynamics. """
@@ -351,9 +539,6 @@ def single_ellipse_hull_linear_triple_plot(save_figure=False, n_resolution=40):
         figure_name = "comparison_linear_hull"
         plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
 
-
-def single_ellipse_hull_nonlinear_triple_plot(save_figure=False, n_resolution=4):
-    pass
 
 def starshape_hull_linear_triple_plot(save_figure=False, n_resolution=20):
     """ Moving inside an 'ellipse hull with linear dynamics. """
@@ -560,7 +745,13 @@ if (__name__)=="__main__":
     # single_ellipse_linear_triple_plot(save_figure=False, n_resolution=100)
     # single_ellipse_nonlinear_triple_plot(save_figure=False)
     
-    single_ellipse_spiral_triple_plot(save_figure=False, n_resolution=100)
+    # single_ellipse_spiral_triple_plot(save_figure=False, n_resolution=100)
+    # single_ellipse_spiral_analysis(save_figure=True, n_resolution=100)
+
+    # TODO: analyse this local-convergence better / what velocity is needed.
+    #         What do we need to know about the field?
+    # single_ellipse_multiattractor_triple_plot(save_figure=True, n_resolution=100)
+    # single_ellipse_multiattractor_analysis(save_figure=False, n_resolution=100)
     
     # single_ellipse_hull_linear_triple_plot(save_figure=True, n_resolution=100)
     # single_ellipse_hull_nonlinear_triple_plot(save_figure=True, n_resolution=100)
@@ -573,3 +764,4 @@ if (__name__)=="__main__":
     
     # multiple_hull_empty(save_figure=True)
     # single_ellipse_hull(save_figure=True)
+    pass
