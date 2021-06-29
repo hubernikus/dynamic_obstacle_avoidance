35d4369af6ab613a286f00909ff4b977eb2dabae
Lukas <huberniku@gmail.com>
2021-06-29 23:29:09 +0200

    display ellipses

--- src/dynamic_obstacle_avoidance/visualization/plot_3d_trajectory.py
+++ src/dynamic_obstacle_avoidance/visualization/plot_3d_trajectory.py
@@ -14,29 +14,46 @@ from mpl_toolkits.mplot3d import axes3d
 
 plt.ion()  # Show plot without stopping code
 
-def plot_obstacles(obstacle_list, fig_and_ax_handle=None):
+
+def plot_obstacles(ObstacleContainer, ax=None):
     """ """ 
-    if fig_and_ax_handle is None:
+    if ax is None:
+        # breakpoint()
         fig = plt.figure()
         ax = fig.gca(projection='3d')
-    else:
-        fig, ax = fig_and_ax_handle
+    # else:
+        # fig, ax = fig_and_ax_handle
+
+    for obs in ObstacleContainer:
+        data_points = obs.draw_obstacle()
+        ax.plot_surface(data_points[0], data_points[1], data_points[2],
+                        rstride=4, cstride=4, color=np.array([176, 124, 124])/255.)
 
 
 def plot_obstacles_and_trajectory_3d(
-    obstacle_container, initial_dynamical_system=,
+    ObstacleContainer,
+    # initial_dynamical_system=,
     start_positions=None,
-    obstacle_avoidance_function=,
-    x_lim=None, y_lim=None,
+    # func_obstacle_avoidance=,
+    x_lim=None, y_lim=None, z_lim=None,
     fig_and_ax_handle=None,
     ):
+
+    dimension = 3 # 3D-visualization
     
     if fig_and_ax_handle is None:
-        fig = plt.figure()
+        fig = plt.figure(figsize=(8, 6))
         ax = fig.gca(projection='3d')
     else:
         fig, ax = fig_and_ax_handle
         
-    plot_obstacles(fig_and_ax_handle=fig_and_ax_handle)
+    plot_obstacles(ObstacleContainer=ObstacleContainer, ax=ax)
     
-    dim = 3 # 3D-visualization
+    # ax.set_aspect('equal')
+    if x_lim is not None:
+        ax.set_xlim(x_lim)
+    if y_lim is not None:
+        ax.set_ylim(y_lim)
+    if z_lim is not None:
+        ax.set_zlim(z_lim)
+
