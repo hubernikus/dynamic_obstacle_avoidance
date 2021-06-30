e7411bf5b3ef6d7ab5122f11302375956e34973a
Lukas <huberniku@gmail.com>
2021-06-30 18:54:04 +0200

    clean test scripts

--- tests/test_ellipse_obstacle.py
+++ tests/test_ellipse_obstacle.py
@@ -1,12 +1,7 @@
-'''
-UNIT TESTING
-
+"""
 Test script for obstacle avoidance algorithm
 Test normal formation
-'''
-
-# TODO: TEST on: moving general creation, moving, gamma values, obstacle container
-
+"""
 import numpy as np
 from math import pi
 
@@ -14,83 +9,98 @@ from dynamic_obstacle_avoidance.obstacle_avoidance.gradient_container import Gra
 from dynamic_obstacle_avoidance.obstacles import Ellipse, CircularObstacle
 from dynamic_obstacle_avoidance.visualization.vector_field_visualization import Simulation_vectorFields
 
-def visualize_ellipse_with_ref_point_inside():
-    ''' Visualize ellipse with reference point far away '''
+class TestEllipses(unittest.TestCase):
+    def test_ellipse_reference_point_inside(self, visualize=False):
+        """ Visualize ellipse with reference point far away """
+        # Create Ellipse
+        obs = Ellipse(
+            axes_length=[1.2, 2.0],
+            center_position=[0.0, 0.0],
+            orientation=30./180*pi,
+        )
 
-    # Create Ellipse
-    obs = Ellipse(
-        axes_length=[1.2, 2.0],
-        center_position=[0.0, 0.0],
-        orientation=30./180*pi,
-    )
+        # Reset reference point
+        obs.set_reference_point(np.array([1, 0.3]), in_global_frame=True)
 
-    # Reset reference point
-    obs.set_reference_point(np.array([1, 0.3]), in_global_frame=True)
+        self.assertTrue(obs.get_gamma(obs.reference_point)>1,
+                            "Gamma of reference point is too small.")
 
-    obs_list = GradientContainer()
-    obs_list.append(obs)
+        obs_list = GradientContainer()
+        obs_list.append(obs)
     
-    Simulation_vectorFields(
-        x_range=[-3, 3], y_range=[-3, 3],
-        point_grid=100,
-        obs=obs_list,
-        draw_vectorField=False,
-        automatic_reference_point=False,
-    )
-
+        if visualize:
+            Simulation_vectorFields(
+                x_range=[-3, 3], y_range=[-3, 3],
+                point_grid=100,
+                obs=obs_list,
+                draw_vectorField=False,
+                automatic_reference_point=False,
+            )
         
-def visualization_ellipse_with_ref_point_outside():
-    ''' Visualize ellipse with reference point far away '''
-    # Create Ellipse
-    obs = Ellipse(
-        axes_length=[1.2, 2.0],
-        center_position=[0.0, 0.0],
-        orientation=30./180*pi,
-    )
+    def test_visualization_ellipse_with_ref_point_outside(self, visualize=False):
+        """ Visualize ellipse with reference point far away """
+        # Create Ellipse
+        obs = Ellipse(
+            axes_length=[1.2, 2.0],
+            center_position=[0.0, 0.0],
+            orientation=30./180*pi,
+        )
 
+        # Set reference point outside
+        obs.set_reference_point(np.array([2, 1]), in_global_frame=True)
 
-    # Set reference point outside
-    obs.set_reference_point(np.array([2, 1]), in_global_frame=True)
-    
-    obs_list = GradientContainer()
-    obs_list.append(obs)
+        obs_list = GradientContainer()
+        obs_list.append(obs)
 
-    Simulation_vectorFields(
-        x_range=[-3, 3], y_range=[-3, 3],
-        point_grid=0,
-        obs=obs_list,
-        draw_vectorField=False,
-        automatic_reference_point=False,
-    )
+        if visualize:
+            Simulation_vectorFields(
+                x_range=[-3, 3], y_range=[-3, 3],
+                point_grid=0,
+                obs=obs_list,
+                draw_vectorField=False,
+                automatic_reference_point=False,
+            )
 
-    
-def visualization_circular_reference_point_outside():
-    ''' Visualize circular-obstacle with reference point far away '''
+    def test_visualization_circular_reference_point_outside(self, visualize):
+        """ Visualize circular-obstacle with reference point far away """
+        obs = CircularObstacle(
+            radius=1.5,
+            center_position=[0.0, 0.0],
+            orientation=0./180*pi,
+        )
 
-    obs = CircularObstacle(
-        radius=1.5,
-        center_position=[0.0, 0.0],
-        orientation=0./180*pi,
-    )
+        obs.set_reference_point(np.array([1.2, 1.9]), in_global_frame=True)
 
-    obs.set_reference_point(np.array([1.2, 1.9]), in_global_frame=True)
+        obs_list = GradientContainer()
+        obs_list.append(obs)
 
-    obs_list = GradientContainer()
-    obs_list.append(obs)
-    
-    Simulation_vectorFields(
-        x_range=[-3, 3], y_range=[-3, 3],
-        point_grid=0,
-        obs=obs_list,
-        draw_vectorField=False,
-        automatic_reference_point=False,        
-    )
+        if visualize:
+            Simulation_vectorFields(
+                x_range=[-3, 3], y_range=[-3, 3],
+                point_grid=0,
+                obs=obs_list,
+                draw_vectorField=False,
+                automatic_reference_point=False,        
+            )
+
+    def test_creation_3d(self):
+        ObstacleEnvironment = RotationContainer()
+        ObstacleEnvironment.append(
+            Ellipse(
+            center_position=np.array([0.5, -1, 0.3]), 
+            axes_length=np.array([0.3, 0.3, 0.3]),
+            orientation=Rotation.from_rotvec([0, 0, 0]),
+            tail_effect=False,
+            )
+        )
 
     
 if (__name__) == "__main__":
-    # test_normal_ellipse()
-    # visualize_ellipse_with_ref_point_inside()
-    # visualization_ellipse_with_ref_point_outside()
-    visualization_circular_reference_point_outside()
-    
-    print("Selected tests complete.")
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+
+    visualize = False
+    if visualize:
+        Tester = TestEllipses()
+        Tester.test_visualization_ellipse_with_ref_point_outside(visualize=visualize)
+        Tester.test_ellipse_reference_point_inside(visualize=visualize)
+        Tester.test_visualization_circular_reference_point_outside(visualize=visualize)
--- tests/test_multiple_obstacles.py
+++ tests/test_multiple_obstacles.py
@@ -1,9 +1,9 @@
-'''
+"""
 UNIT TESTING
 
 Test script for obstacle avoidance algorithm
 Test normal formation
-'''
+"""
 
 # TODO: TEST on: moving general creation, moving, gamma values, obstacle container
 import numpy as np
@@ -15,105 +15,123 @@ from dynamic_obstacle_avoidance.obstacle_avoidance.gradient_container import Gra
 
 from dynamic_obstacle_avoidance.visualization.vector_field_visualization import Simulation_vectorFields
 
+class TestMultipleObstacles(unittest.TestCase):
+    def test_two_intersecting_circles(self, visualize=False):
+        """ Appending one obstacle. """
+        obs_list = GradientContainer() # create empty obstacle list
+        obs_list.append(CircularObstacle(
+                radius=1.5,
+                center_position=[-1.0, 0.0],
+                orientation=0./180*pi,
+        ))                          #
 
-def test_two_intersecting_circles():
-    ''' Appending one obstacle. '''
+        obs_list.append(CircularObstacle(
+                radius=1.5,
+                center_position=[1.0, 0.0],
+                orientation=0./180*pi,
+        ))                          #
+
+        obs_list.update_reference_points()
+
+        if visualize:
+            Simulation_vectorFields(
+                x_range=[-4, 4], y_range=[-4, 4],
+                point_grid=0,
+                obs=obs_list,
+                automatic_reference_point=False,        
+            )
+
+        for obs in obs_list:
+            self.assertTrue(obs.get_gamma(obs.reference_point)<0,
+                            "Warning reference point outside obstacle")
+            self.assertTrue(obs.get_gamma(
+                obs.global_reference_point, in_global_frame=True)<0,
+                            "Warning reference point outside obstacle")
+        
+    def test_three_intersecting_circles(self, visualize=False):
+        """ Appending one obstacle. """
+        obs_list = GradientContainer() # create empty obstacle list
+        obs_list.append(CircularObstacle(
+                radius=1.0,
+                center_position=[-1.2, 0.5],
+                orientation=0./180*pi,
+        ))                          #
+
+        obs_list.append(CircularObstacle(
+                radius=1.0,
+                center_position=[0.0, 0.0],
+                orientation=0./180*pi,
+        ))                          #
+
+        obs_list.append(CircularObstacle(
+                radius=1.0,
+                center_position=[1.2, 1.5],
+                orientation=0./180*pi,
+        ))                          #
+
+        obs_list.update_reference_points()
+
+        if visualize:
+            Simulation_vectorFields(
+                x_range=[-4, 4], y_range=[-4, 4],
+                point_grid=0,
+                obs=obs_list,
+                automatic_reference_point=False,        
+            )
+
+        for obs in obs_list:
+            self.assertTrue(obs.get_gamma(obs.reference_point)<0,
+                            "Warning reference point outside obstacle")
+            self.assertTrue(obs.get_gamma(
+                obs.global_reference_point, in_global_frame=True)<0,
+                            "Warning reference point outside obstacle for global-frame-representation.")
+
+
+    def test_two_intersecting_ellipses(self, visualize=False):
+        """ Appending one obstacle. """
+        obs_list = GradientContainer() # create empty obstacle list
+        obs_list.append(Ellipse(
+                axes_length=np.array([1, 2]),
+                center_position=[-1.2, 0.0],
+                orientation=-40./180*pi,
+        ))                          #
+
+        obs_list.append(Ellipse(
+                axes_length=np.array([1, 2]),
+                center_position=[1.5, 0.0],
+                orientation=40./180*pi,
+        ))                          #
+        # obs_list.append(CircularObstacle(
+                # radius=1.0,
+                # center_position=[1.2, 1.5],
+                # orientation=0./180*pi,
+        # ))                          #
+
+        obs_list.update_reference_points()
+        
+        if visualize:
+            Simulation_vectorFields(
+                x_range=[-4, 4], y_range=[-4, 4],
+                point_grid=0,
+                obs=obs_list,
+                automatic_reference_point=False,        
+                )
+        for obs in obs_list:
+            self.assertTrue(obs.get_gamma(obs.reference_point)<0,
+                            "Warning reference point outside obstacle")
+            self.assertTrue(obs.get_gamma(
+                obs.global_reference_point, in_global_frame=True)<0,
+                            "Warning reference point outside obstacle")
+
     
-    obs_list = GradientContainer() # create empty obstacle list
-    obs_list.append(CircularObstacle(
-            radius=1.5,
-            center_position=[-1.0, 0.0],
-            orientation=0./180*pi,
-    ))                          #
-
-    obs_list.append(CircularObstacle(
-            radius=1.5,
-            center_position=[1.0, 0.0],
-            orientation=0./180*pi,
-    ))                          #
-
-    obs_list.update_reference_points()
-
-    Simulation_vectorFields(
-        x_range=[-4, 4], y_range=[-4, 4],
-        point_grid=0,
-        obs=obs_list,
-        automatic_reference_point=False,        
-    )
-
-
-
-def test_three_intersecting_circles():
-    ''' Appending one obstacle. '''
-    
-    obs_list = GradientContainer() # create empty obstacle list
-    obs_list.append(CircularObstacle(
-            radius=1.0,
-            center_position=[-1.2, 0.5],
-            orientation=0./180*pi,
-    ))                          #
-
-    obs_list.append(CircularObstacle(
-            radius=1.0,
-            center_position=[0.0, 0.0],
-            orientation=0./180*pi,
-    ))                          #
-
-    obs_list.append(CircularObstacle(
-            radius=1.0,
-            center_position=[1.2, 1.5],
-            orientation=0./180*pi,
-    ))                          #
-
-    obs_list.update_reference_points()
-    
-    Simulation_vectorFields(
-        x_range=[-4, 4], y_range=[-4, 4],
-        point_grid=0,
-        obs=obs_list,
-        automatic_reference_point=False,        
-    )
-
-
-
-def test_two_intersecting_ellipses():
-    ''' Appending one obstacle. '''
-    
-    obs_list = GradientContainer() # create empty obstacle list
-    obs_list.append(Ellipse(
-            axes_length=np.array([1, 2]),
-            center_position=[-1.2, 0.0],
-            orientation=-40./180*pi,
-    ))                          #
-
-    obs_list.append(Ellipse(
-            axes_length=np.array([1, 2]),
-            center_position=[1.5, 0.0],
-            orientation=40./180*pi,
-    ))                          #
-
-    # obs_list.append(CircularObstacle(
-            # radius=1.0,
-            # center_position=[1.2, 1.5],
-            # orientation=0./180*pi,
-    # ))                          #
-
-    obs_list.update_reference_points()
-    
-    Simulation_vectorFields(
-        x_range=[-4, 4], y_range=[-4, 4],
-        point_grid=0,
-        obs=obs_list,
-        automatic_reference_point=False,        
-    )
-
-    
-
 if (__name__)=="__main__":
-    # test_two_intersecting_circles()
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
 
-    # test_three_intersecting_circles()
-
-    test_two_intersecting_ellipses()
+    plot_results = False
+    if plot_results:
+        Tester = TestMultipleObstacles()
+        Tester.test_two_intersecting_circles(plot_results)
+        Tester.test_three_intersecting_circles(plot_results)
+        Tester.test_two_intersecting_ellipses(plot_results)
 
     print("Selected tests complete.")
