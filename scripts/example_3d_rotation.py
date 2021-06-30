19e84939584a46a893de9e19cedfd70407f1b58e
Lukas <huberniku@gmail.com>
2021-06-30 18:53:23 +0200

    rename and move files

--- scripts/example_3d_rotation.py
+++ scripts/example_3d_rotation.py
@@ -18,7 +18,19 @@ from dynamic_obstacle_avoidance.avoidance import obstacle_avoidance_rotational
 from dynamic_obstacle_avoidance.containers import MultiBoundaryContainer, RotationContainer
 from dynamic_obstacle_avoidance.visualization.plot_3d_trajectory import plot_obstacles_and_trajectory_3d
 
-def test_single_ellipse_3d():
+
+def evaluate_single_position(
+    position,
+    InitialDynamics,
+    ObstacleContainer,
+    func_obstacle_avoidance):
+    
+    initial_velocity = InitialDynamics.evaluate(position)
+    modulated_velocity = func_obstacle_avoidance(position, initial_velocity, ObstacleContainer)
+    
+    return modulated_velocity
+
+def example_single_ellipse_3d():
     ObstacleEnvironment = RotationContainer()
     # ObstacleEnvironment.append(
     #     Ellipse(
@@ -45,11 +57,19 @@ def test_single_ellipse_3d():
     #     tail_effect=False,
     #     )
     # )
-    ObstacleEnvironment = RotationContainer()
+    # ObstacleEnvironment = RotationContainer()
     
     InitialDynamics = SpiralStable()
     ObstacleEnvironment.set_convergence_directions(
         ConvergingDynamics=LinearSystem(InitialDynamics.attractor_position))
+
+    # test_pos = np.array([ 0.24685765, -0.49288152, -0.35809686])
+    # evaluate_single_position(test_pos,
+    #                          InitialDynamics=InitialDynamics,
+    #                          ObstacleContainer=ObstacleEnvironment,
+    #                          func_obstacle_avoidance=obstacle_avoidance_rotational)
+    # if True:
+        # return 
 
     # start_pos1 = [0.1, 0.1, 0.1]
     start_pos1 = [1.4, 1.4, 1.0]
@@ -67,4 +87,4 @@ def test_single_ellipse_3d():
                           # x_lim=[-2, 2], y_lim=[-2, 2], z_lim=[-2, 2])
     
 if (__name__)=="__main__":
-    test_single_ellipse_3d()
+    example_single_ellipse_3d()
---
+++ scripts/visualize_rotation_weight.py
@@ -0,0 +1,65 @@
+#!/USSR/bin/python3.9
+""" Test the directional space.
+Creates graph of the rotational/directional weight."""
+# Author: Lukas Huber
+# Email: lukas.huber@epfl.ch
+# Created: 2021-05-18
+# License: BSD (c) 2021
+
+import unittest
+import numpy as np
+
+def weight_invgamma(inv_gamma, pow_fac):
+    return weight ** pow_fac
+
+def weight_dist(dist, inv_gamma, pow_fac):
+    return (1.0/(1- dist/(1-inv_gamma)))
+
+class TestRotational(unittest.TestCase):
+    def rotation_weight(self, save_figure=True):
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D  
+        from matplotlib import cm
+
+        import numpy as np
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        n_grid = 20
+        dist0 = np.linspace(1e-6, 1-1e-6, n_grid)
+        weight = np.linspace(1e-6, 1-1e-6, n_grid)
+        weight, dist0 = np.meshgrid(weight, dist0)
+        gamma = 1./weight
+
+        # Make data.
+        pow_factor = 3.0
+        val = weight ** (1.0/(pow_factor*dist0))
+        surf = ax.plot_surface(dist0, weight, val,
+                               cmap=cm.YlGnBu,
+                               linewidth=0.2, edgecolors='k')
+                               # antialiased=False)
+        # breakpoint()
+        import matplotlib as mpl
+        mpl.rc('font', family='Times New Roman')
+        ax.set_xlabel(r'Relative Rotation $\tilde d (\xi)$')
+        ax.set_ylabel(r'Weight $1/\Gamma(\xi)$')
+        ax.set_zlabel(r'Rotational Weights $w_r(\Gamma, \tilde{d})$')
+        ax.set_xlim([0, 1])
+        ax.set_ylim([0, 1])
+        ax.set_zlim([0, 1])
+        print("Done")
+
+        if save_figure:
+            figure_name = "rotational_weight_with_power_" + str(int(pow_factor))
+            plt.savefig("figures/" + figure_name + ".png", bbox_inches='tight')
+
+        plt.ion()
+        plt.show()
+        # plt.close('all')
+        
+if __name__ == '__main__':
+    visualize = True
+    if visualize:
+        Tester = TestRotational()
+        Tester.rotation_weight(save_figure=True)
