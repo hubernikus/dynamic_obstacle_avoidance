cd8d731fe10b8250a019ae97b55a05cd30a4c170
Lukas <huberniku@gmail.com>
2021-08-18 12:22:13 +0200

    working star-2-sphere-space trafo

--- analysis/comparison_navigation/main.py
+++ analysis/comparison_navigation/main.py
@@ -4,6 +4,8 @@ Replicating the paper of
 """
 from dataclasses import dataclass
 
+from math import pi
+
 import numpy as np
 from numpy import linalg as LA
 
@@ -39,7 +41,6 @@ class NavigationContainer(BaseContainer):
     q: ray-center
     rho: minimum radius
     """
-
     @property
     def attractor_position(self):
         return self._attractor_position
@@ -59,7 +60,7 @@ class NavigationContainer(BaseContainer):
         
     def get_analytic_switches(self, position, beta_values, lambda_constant=1):
         """ Analytic switches of obstacle-avoidance function. """
-        ind_zero = beta_values == 0
+        ind_zero = np.isclose(beta_values, 0)
         if np.sum(ind_zero) == 0:
             beta_bar = beta_values / np.tile(np.prod(beta_values), (beta_values.shape[0]))
         elif np.sum(ind_zero) == 1:
@@ -68,13 +69,13 @@ class NavigationContainer(BaseContainer):
         else:
             raise Exception("Two zero-value beta's detected. This indicates an invalid \n"
                             + "environment of intersecting obstacles.")
-
+        
         # Or gamma_d
         rel_dist_attractor = LA.norm(self.get_relative_attractor_position(position))
 
         scaled_dist = beta_bar*rel_dist_attractor
         switch_value = scaled_dist / (scaled_dist + lambda_constant*beta_values)
-
+        
         return switch_value
     
     def transform_to_sphereworld(self, position):
@@ -82,16 +83,15 @@ class NavigationContainer(BaseContainer):
         # rel_pos_obstacle = np.zeros((self.dimension, self.n_obstacles))
         position_starshape = np.zeros(position.shape)
         beta_values = self.get_beta_values(position)
+        analytic_switches = self.get_analytic_switches(position, beta_values=beta_values)
         
         for ii in range(self.n_obstacles):
             rel_obs_position = position - self[ii].position
             
-            analytic_switch = self.get_analytic_switches(position, beta_values=beta_values)
             mu = get_starset_deforming_factor(
                 self[ii], beta=beta_values[ii], rel_obs_position=rel_obs_position)
-            
-            position_starshape += analytic_switch*(mu*rel_obs_position + self[ii].position)
 
+            position_starshape += analytic_switches[ii]*(mu*rel_obs_position + self[ii].position)
         return position_starshape
 
 
@@ -104,11 +104,27 @@ def plot_star_and_sphere_world():
     obstacle_container.attractor_position = np.array([0, 5])
     obstacle_container.append(
         Ellipse(
-            center_position=np.array([0, 0]), 
+            center_position=np.array([2, 0]), 
             axes_length=np.array([2, 1]),
             )
         )
-    
+
+    obstacle_container.append(
+        Ellipse(
+            center_position=np.array([-2, 2]), 
+            axes_length=np.array([2, 1]),
+            orientation=30./180*pi,
+            )
+        )
+
+    obstacle_container.append(
+        Ellipse(
+            center_position=np.array([-1, -2.5]), 
+            axes_length=np.array([2, 1]),
+            orientation=-50./180*pi,
+            )
+        )
+
     n_resolution = 50
     # velocities = np.zeros(positions.shape)
 
@@ -128,21 +144,21 @@ def plot_star_and_sphere_world():
                 
             # collisions[ii] = obstacle_container.check_collision(positions[:, ii])
             
-        ax.plot(boundary_displaced[0, :], boundary_displaced[1, :], 'k')
+        ax.plot(boundary_displaced[0, :], boundary_displaced[1, :], 'b')
+
         ax.plot(obstacle_container[it_obs].center_position[0],
-                obstacle_container[it_obs].center_position[1], 'k+')
-    
+                obstacle_container[it_obs].center_position[1], 'b+')
+            
     ax.set_xlim(x_lim)
     ax.set_ylim(y_lim)
 
     ax.plot(obstacle_container.attractor_position[0],
-            obstacle_container.attractor_position[1], 'k+')
+            obstacle_container.attractor_position[1], 'k*')
     
     ax.set_aspect('equal', adjustable='box')
     
     plt.ion()
     plt.show()
-    breakpoint()
 
     # cbar = fig.colorbar(cs, ticks=np.linspace(-10, 0, 11))
     
