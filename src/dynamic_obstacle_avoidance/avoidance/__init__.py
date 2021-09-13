ba0b5a89792ffac07590ba9334df3f15390216d2
Lukas <huberniku@gmail.com>
2021-09-13 13:25:47 +0200

    Create avoider class

--- src/dynamic_obstacle_avoidance/avoidance/__init__.py
+++ src/dynamic_obstacle_avoidance/avoidance/__init__.py
@@ -9,6 +9,10 @@ from .repulsion_modulation import obs_avoidance_nonlinear_hirarchy
 from .comparison_algorithms import obs_avoidance_potential_field, obs_avoidance_orthogonal_moving
 from .rotation import obstacle_avoidance_rotational
 
+# Avoider Classes
+from .obstacle_avoider import ObstacleAvoiderWithInitialDynamcis, DynamicModulationAvoider
+
+# Addition classes / functions
 from .rk4 import obs_avoidance_rk4, obs_avoidance_rungeKutta
 
 __all__ = ['obs_avoidance_rk4',
@@ -18,4 +22,6 @@ __all__ = ['obs_avoidance_rk4',
            'obs_avoidance_nonlinear_hirarchy',
            'obs_avoidance_potential_field',
            'obs_avoidance_orthogonal_moving',
+           'ObstacleAvoiderWithInitialDynamcis',
+           'DynamicModulationAvoider',
            ]
