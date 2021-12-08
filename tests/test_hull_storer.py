efce84ad78b25e4c2cb765bc3a68d4be5d2fdf13
Lukas <huberniku@gmail.com>
2021-12-08 14:33:12 +0100

    hull storer working

---
+++ tests/test_hull_storer.py
@@ -0,0 +1,9 @@
+""" Test the drawing of different obstacles."""
+# Author: Lukas Huber
+# Created: 2021-11-09
+# License: BSD (c) 2021
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from dynamic_obstacle_avoidance.obstacles import HullStorer
