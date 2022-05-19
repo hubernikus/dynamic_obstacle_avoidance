fix angular velocity for zero rotation

dynamic_obstacle_avoidance/utils.py
@@ -83,13 +83,20 @@ def get_relative_obstacle_velocity(
                 )
                 xd_w = xd_w[0:2]
         elif dim == 3:
-            xd_w = np.cross(
-                obs[it_obs].angular_velocity,
-                position - obs[it_obs].center_position,
-            )
+            if (obs[it_obs].angular_velocity is not None
+                and LA.norm(obs[it_obs].angular_velocity)):
+                xd_w = np.cross(
+                    obs[it_obs].angular_velocity,
+                    position - obs[it_obs].center_position,
+                )
+            else:
+                xd_w = np.zeros(dim)
         else:
+            if (obs[it_obs].angular_velocity is not None
+                and LA.norm(obs[it_obs].angular_velocity)):
+                warnings.warn("Angular velocity is not defined for={}".format(d))
             xd_w = np.zeros(dim)
-            warnings.warn("Angular velocity is not defined for={}".format(d))
+            
 
         weight_angular = np.exp(-1.0 * (np.max([gamma_list[ii], 1]) - 1))
 