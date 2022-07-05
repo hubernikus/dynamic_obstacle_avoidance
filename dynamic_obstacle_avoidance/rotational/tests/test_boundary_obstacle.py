cfc3c09753872bcb30cf19cd4defdf4d257fb79b
Lukas Huber <hubernikus@gmail.com>
2022-07-05 15:06:03 +0200

    Working multiobstacle + plot

--- dynamic_obstacle_avoidance/rotational/tests/test_boundary_obstacle.py
+++ dynamic_obstacle_avoidance/rotational/tests/test_boundary_obstacle.py
@@ -268,7 +268,37 @@ def test_obstacle_without_interior(visualize=False):
         ), "Vector not pointing in the correct direction."
 
 
-def test_shortes_path(visualize=False):
+def test_shortes_path(visualize=False, savefig=False):
+    # outer_obstacle = Cuboid(
+    #     center_position=np.array([0, 0]),
+    #     axes_length=np.array([2, 2]),
+    #     is_boundary=False,
+    # )
+
+    # subhull = []
+    # subhull.append(
+    #     Ellipse(
+    #         center_position=np.array([0.8, 0]),
+    #         axes_length=np.array([1.5, 1.0]),
+    #         is_boundary=True,
+    #     )
+    # )
+
+    # subhull.append(
+    #     Ellipse(
+    #         center_position=np.array([-0.2, 0.1]),
+    #         axes_length=np.array([1.0, 1.2]),
+    #         is_boundary=True,
+    #     )
+    # )
+
+    # my_hullobstacle = MultiHullAndObstacle(
+    #     outer_obstacle=outer_obstacle, inner_obstacles=subhull
+    # )
+
+    # my_hullobstacle.evaluate_hirarchy_and_reference_points()
+    # my_hullobstacle.set_attractor(np.array([0, 0]))
+
     outer_obstacle = Cuboid(
         center_position=np.array([0, 0]),
         axes_length=np.array([2, 2]),
@@ -278,8 +308,8 @@ def test_shortes_path(visualize=False):
     subhull = []
     subhull.append(
         Ellipse(
-            center_position=np.array([0.8, 0]),
-            axes_length=np.array([1.5, 1.0]),
+            center_position=np.array([0.5, -0.4]),
+            axes_length=np.array([1.5, 0.5]),
             is_boundary=True,
         )
     )
@@ -297,13 +327,16 @@ def test_shortes_path(visualize=False):
     )
 
     my_hullobstacle.evaluate_hirarchy_and_reference_points()
-    my_hullobstacle.set_attractor(np.array([0, 0]))
+    my_hullobstacle.set_attractor(np.array([-0.3, 0.4]))
 
     if visualize:
         n_resolution = 50
 
-        x_lim = [-1.5, 1.5]
-        y_lim = [-1.5, 1.5]
+        # x_lim = [-1.5, 1.5]
+        # y_lim = [-1.5, 1.5]
+
+        x_lim = [-2.0, 2.0]
+        y_lim = [-2.0, 2.0]
 
         n_x = n_y = n_resolution
         x_vals, y_vals = np.meshgrid(
@@ -311,8 +344,8 @@ def test_shortes_path(visualize=False):
             np.linspace(y_lim[0], y_lim[1], n_y),
         )
 
-        position = np.array([0.6, -0.4])
-        velocity = my_hullobstacle.evaluate(position=position)
+        # position = np.array([0.6, -0.4])
+        # velocity = my_hullobstacle.evaluate(position=position)
 
         positions = np.vstack((x_vals.reshape(1, -1), y_vals.reshape(1, -1)))
         velocities = np.zeros(positions.shape)
@@ -321,23 +354,86 @@ def test_shortes_path(visualize=False):
         n_obs = 3
         weights = np.zeros((n_obs, positions.shape[1]))
 
+        velocities_partial = np.zeros((positions.shape[0], positions.shape[1], n_obs))
+
         for ii in range(positions.shape[1]):
             velocities[:, ii] = my_hullobstacle.evaluate(position=positions[:, ii])
             weights[:, ii] = my_hullobstacle.weights
+
+            obs_hashes = my_hullobstacle.inner_obstacles + [None]
+            for ind, obs_hash in enumerate(obs_hashes):
+                if my_hullobstacle.weights[ind]:
+                    velocities_partial[
+                        :, ii, ind
+                    ] = my_hullobstacle._get_local_dynamics(
+                        positions[:, ii],
+                        obs_hash=obs_hash,
+                        gamma=my_hullobstacle.gamma_list[ind],
+                    )
 
         fig, ax = plt.subplots(1, 1, figsize=(8, 6))
         my_hullobstacle.plot_obstacle(
             x_lim=x_lim, y_lim=y_lim, ax=ax, plot_attractors=True
         )
 
-        ax.quiver(
-            positions[0, :],
-            positions[1, :],
-            velocities[0, :],
-            velocities[1, :],
-            color="k",
+        if savefig:
+            figname = "clustering_obstacles"
+            plt.savefig("figures/" + figname + ".pdf", bbox_inches="tight")
+
+        # ax.quiver(
+        #     positions[0, :],
+        #     positions[1, :],
+        #     velocities[0, :],
+        #     velocities[1, :],
+        #     color="k",
+        #     zorder=4,
+        # )
+
+        ax.streamplot(
+            positions[0, :].reshape(n_x, n_y),
+            positions[1, :].reshape(n_x, n_y),
+            velocities[0, :].reshape(n_x, n_y),
+            velocities[1, :].reshape(n_x, n_y),
+            color="blue",
             zorder=4,
         )
+
+        if savefig:
+            figname = "moving_outside_to_inside"
+            plt.savefig("figures/" + figname + ".pdf", bbox_inches="tight")
+
+        # _, axs = plt.subplots(1, my_hullobstacle.n_elements, figsize=(15, 3.5))
+
+        for oo in range(my_hullobstacle.n_elements):
+            fig, ax = plt.subplots(1, 1, figsize=(8, 7))
+            # axs[oo].quiver(
+            ax.quiver(
+                positions[0, :],
+                positions[1, :],
+                velocities_partial[0, :, oo],
+                velocities_partial[1, :, oo],
+                # color="k",
+                color="blue",
+                zorder=4,
+                scale=45,
+            )
+
+            # ax.streamplot(
+            #     positions[0, :].reshape(n_x, n_y),
+            #     positions[1, :].reshape(n_x, n_y),
+            #     velocities_partial[0, :, oo].reshape(n_x, n_y),
+            #     velocities_partial[1, :, oo].reshape(n_x, n_y),
+            #     color="blue",
+            #     zorder=4,
+            # )
+
+            my_hullobstacle.plot_obstacle(
+                x_lim=x_lim, y_lim=y_lim, ax=ax, plot_attractors=True
+            )
+
+            if savefig:
+                figname = f"moving_outside_partial_{oo}"
+                plt.savefig("figures/" + figname + ".pdf", bbox_inches="tight")
 
         # Weights
         # fig, axs = plt.subplots(1, my_hullobstacle.n_elements, figsize=(15, 3.5))
@@ -359,15 +455,102 @@ def test_shortes_path(visualize=False):
         #         y_lim=y_lim,
         #         ax=axs[oo],
         #     )
-
         # plt.colorbar(cs0, ax=axs)
+
+
+def _test_multiholes_obstacle(visualize=False, savefig=False):
+    # TODO: this does not work yet (!)
+    outer_obstacle = Cuboid(
+        center_position=np.array([0, 0]),
+        axes_length=np.array([2, 3]),
+        is_boundary=False,
+    )
+
+    subhull = []
+    subhull.append(
+        Ellipse(
+            center_position=np.array([-0.5, 1.0]),
+            axes_length=np.array([1.5, 0.5]),
+            is_boundary=True,
+        )
+    )
+
+    subhull.append(
+        Ellipse(
+            center_position=np.array([0.5, -0.3]),
+            axes_length=np.array([1.4, 1.2]),
+            is_boundary=True,
+        )
+    )
+
+    my_hullobstacle = MultiHullAndObstacle(
+        outer_obstacle=outer_obstacle, inner_obstacles=subhull
+    )
+
+    if visualize:
+        my_hullobstacle.evaluate_hirarchy_and_reference_points()
+        my_hullobstacle.set_attractor(np.array([-0.3, 0.4]))
+
+        x_lim = [-2.0, 2.0]
+        y_lim = [-2.0, 2.0]
+
+        fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+        my_hullobstacle.plot_obstacle(
+            x_lim=x_lim, y_lim=y_lim, ax=ax, plot_attractors=True
+        )
+
+        n_resolution = 50
+
+        n_x = n_y = n_resolution
+        x_vals, y_vals = np.meshgrid(
+            np.linspace(x_lim[0], x_lim[1], n_x),
+            np.linspace(y_lim[0], y_lim[1], n_y),
+        )
+
+        positions = np.vstack((x_vals.reshape(1, -1), y_vals.reshape(1, -1)))
+        velocities = np.zeros(positions.shape)
+
+        # Weight container for all obstacles
+        n_obs = 3
+        weights = np.zeros((n_obs, positions.shape[1]))
+
+        velocities_partial = np.zeros((positions.shape[0], positions.shape[1], n_obs))
+
+        for ii in range(positions.shape[1]):
+            velocities[:, ii] = my_hullobstacle.evaluate(position=positions[:, ii])
+            weights[:, ii] = my_hullobstacle.weights
+
+            obs_hashes = my_hullobstacle.inner_obstacles + [None]
+            for ind, obs_hash in enumerate(obs_hashes):
+                if my_hullobstacle.weights[ind]:
+                    velocities_partial[
+                        :, ii, ind
+                    ] = my_hullobstacle._get_local_dynamics(
+                        positions[:, ii],
+                        obs_hash=obs_hash,
+                        gamma=my_hullobstacle.gamma_list[ind],
+                    )
+
+        ax.streamplot(
+            positions[0, :].reshape(n_x, n_y),
+            positions[1, :].reshape(n_x, n_y),
+            velocities[0, :].reshape(n_x, n_y),
+            velocities[1, :].reshape(n_x, n_y),
+            color="blue",
+            zorder=4,
+        )
+
+        if savefig:
+            figname = "obstacle_with_mulitholes"
+            plt.savefig("figures/" + figname + ".pdf", bbox_inches="tight")
 
 
 if (__name__) == "__main__":
     plt.close("all")
     # test_boundary_obstacle_weight(visualize=True)
     # test_mixed_boundary_obstacle_reference(visualize=True)
+    # test_shortes_path(visualize=True, savefig=False)
 
-    test_shortes_path(visualize=True)
+    _test_multiholes_obstacle(visualize=True, savefig=True)
 
     # test_obstacle_without_interior(visualize=True)
