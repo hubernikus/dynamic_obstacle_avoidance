1045897bdc93a3a051a7c568bb65ae42b6ab9979
Lukas Huber <hubernikus@gmail.com>
2022-09-05 23:38:26 +0200

    Create multi-output SVR regression class.

--- dynamic_obstacle_avoidance/rotational/tests/test_nonlinear_deviation.py
+++ dynamic_obstacle_avoidance/rotational/tests/test_nonlinear_deviation.py
@@ -16,12 +16,15 @@ from numpy import linalg as LA
 
 import matplotlib.pyplot as plt
 
+from sklearn.svm import SVR
+
 from vartools.linalg import get_orthogonal_basis
 from vartools.dynamical_systems import DynamicalSystem, ConstantValue
 
 # from vartools.directional_space import get_angle_space_of_array
 # from vartools.directional_space import get_angle_space
 from vartools.directional_space import get_angle_space_inverse
+from vartools.directional_space import get_angle_space, get_angle_space_of_array
 
 from dynamic_obstacle_avoidance.rotational.datatypes import Vector, VectorArray
 
@@ -32,18 +35,30 @@ DeviationVector = np.ndarray
 # - directly incoorporate (rotational + fast)) obstacle avoidance as a function
 
 
+class MultiOutputSVR:
+    """Creates several svr-models to predict multi-dimensional output."""
+
+    def __init__(self, **kwargs):
+        # Temporary store keyword arguments for later construction.
+        self._kwargs = kwargs
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        self.n_features_in_ = X.shape[1]
+        self.n_features_out_ = y.shape[1]
+
+        self._models = [SVR(**self._kwargs) for _ in range(self.n_features_out_)]
+
+        for ii, svr in enumerate(self._models):
+            svr.fit(X, y[:, ii])
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        return np.array([svr.predict(X) for svr in self._models])
+
+
 class DirectionalSystem(ABC):
-    def __init__(self):
-        self.base = get_orthogonal_basis(
-            reference_dynamics.evalute(np.zeros(reference_dynamics.dimensions))
-        )
-
-    def dimension(self) -> int:
-        return self.reference_dynamics.dimension
-
     def evaluate(self, position: Vector) -> Vector:
         angle_deviation = self.evaluate_deviation(position)
-        
+
         return get_angle_space_inverse(angle_deviation, self.base)
 
     @abstractmethod
@@ -52,24 +67,24 @@ class DirectionalSystem(ABC):
 
 
 class DeviationOfConstantFlow(DirectionalSystem):
-    maximal_deviation = pi
+    maximal_deviation = 0.99 * pi / 2.0
 
     def __init__(self, reference_velocity: np.ndarray, regressor) -> None:
         self.reference_velocity = reference_velocity
         self.base = get_orthogonal_basis(
-            reference_direction / LA.norm(reference_direction)
+            reference_velocity / LA.norm(reference_velocity)
         )
 
         self.regressor = regressor
 
-    @attribute
+    @property
     def dimension(self):
         return self.reference_velocity.shape[0]
 
-    def fit_from_directions(self, X, np.ndarray, y_directions: np.ndarray) -> None:
+    def fit_from_velocities(self, X: np.ndarray, y_directions: np.ndarray) -> None:
         y = self._clean_input_data(y_directions)
         self.fit(X, y)
-        
+
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """Fits the internal learner onto the given model."""
         self.n_samples_fit_ = X.shape[0]
@@ -80,25 +95,55 @@ class DeviationOfConstantFlow(DirectionalSystem):
         self.n_features_in_ = X.shape[1]
         self.n_features_out_ = y.shape[1]
 
-        if self.n_features_out_ != self.dimension-1:
+        if self.n_features_out_ != self.dimension - 1:
             raise ValueError("Unexpected number of output features.")
 
         self.regressor.fit(X, y)
 
     def _clean_input_data(self, data):
-        pass
-        
-    def evaluate_deviation(self, position: Vector) -> DevationVector:
-        return self.regressor.predict(position.reshape(1, -1))[0]
+        # TODO: use directly the array -> get_angle_space_of_array()
+        directions = np.zeros((data.shape[0], data.shape[1] - 1))
+        for ii in range(data.shape[0]):
+            directions[ii, :] = get_angle_space(data[ii, :], null_matrix=self.base)
+        return directions
+
+    def evaluate_deviation(self, position: Vector) -> np.ndarray:
+        """Return deviation, while making sure that the deviation stays within the
+        feasible boundaries."""
+        deviation = self.regressor.predict(position.reshape(1, -1))[0]
+        if (dev_norm := LA.norm(deviation)) > self.maximal_deviation:
+            deviation = deviation / dev_norm
+        return deviation
 
 
-def test_learn_sinus_motion(visualize=True, save_figure=False):
+def test_learn_sinus_motion(visualize=False, save_figure=False):
+    # Data going from right to left
+    np.random.seed(1)
 
-    dynamics= DeviationOfConstantFlow()
-    pass
+    n_points = 1000
+    x = np.linspace(0, 4 * pi, n_points)
+    y = np.cos(x)
+
+    X = np.vstack((x, y)).T
+    X = X + np.random.rand(X.shape[0], X.shape[1]) * 0.1
+
+    # Obtain velocities form position, and make sure they have the same dimensions
+    velocities = X[1:, :] - X[:-1, :]
+    X = X[:-1, :]
+
+    dynamics = DeviationOfConstantFlow(
+        reference_velocity=np.mean(velocities, axis=0),
+        regressor=MultiOutputSVR(kernel="rbf", gamma=0.1),
+    )
+
+    dynamics.fit_from_velocities(X, velocities)
+
+    if visualize:
+        fig, ax = plt.subplots()
+        ax.plot(X[:, 0], X[:, 1], ".")
 
 
 if (__name__) == "__main__":
 
-    test_learn_sinus_motion()
+    test_learn_sinus_motion(visualize=True)
     print("Tests finished.")
